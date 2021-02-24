0ddaed989ff884d4029f7dc37b3b75e039aa097d
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-02-24 13:56:28 +0300

    edit code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :categories
+  has_many :tasks
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :categories
-  has_many :tasks
 end
